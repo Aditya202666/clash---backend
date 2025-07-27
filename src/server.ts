7710d4f5ae31070842fcde220e9cda962d2af463
Aditya Kumar <aditya202666@gmail.com>
2025-07-27 21:28:08 +0530

    feat: initialize server with Express and Prisma setup
    - Added package.json for project dependencies and scripts.
    
    - Defined Prisma schema for User model.
    - Configured database connection using PrismaClient.
    - Implemented basic Express server with a hello world route.
    - Added TypeScript configuration for the project.

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response, Application} from "express";
+import "dotenv/config";
+
+const app: Application = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
