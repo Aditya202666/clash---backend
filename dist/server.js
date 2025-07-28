1576799215af5179bc277f30ca7550e24109690f
Aditya Kumar <aditya202666@gmail.com>
2025-07-28 11:29:35 +0530

    feat: enhance server setup with EJS view engine and add welcome page

--- dist/server.js
+++ dist/server.js
@@ -1,9 +1,19 @@
 import express from "express";
 import "dotenv/config";
+import path from "path";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = process.env.PORT || 3000;
+// *middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// *view engine
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./views"));
+// *routes
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.render("welcome");
 });
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,34 @@
 import express, { Request, Response, Application} from "express";
 import "dotenv/config";
+import cors from "cors";
+import ejs from "ejs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app: Application = express();
 const port = process.env.PORT || 3000;
 
+
+// *middleware
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// *view engine
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./views"));
+
+
+// *routes
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World!");
+    res.render("welcome");
 });
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
+ 
