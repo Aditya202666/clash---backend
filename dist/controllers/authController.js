a589cf41f6b02a632096df46e53e762f677bbaa4
Aditya Kumar <aditya202666@gmail.com>
2025-07-30 12:09:11 +0530

    feat: comment out payload parsing in registerUser function

--- dist/controllers/authController.js
+++ dist/controllers/authController.js
@@ -1,7 +1,6 @@
-import { registerSchema } from "../validators/authValidators.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 const registerUser = asyncHandler(async (req, res) => {
     const body = req.body;
-    const payload = registerSchema.parse(body);
+    //   const payload = registerSchema.parse(body);
 });
 export { registerUser };
